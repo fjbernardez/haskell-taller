607f48929e1d17a2628d3630d61d40e902898424
fjbernardez <bernardez.f@gmail.com>
2020-04-25 20:47:58 -0300

    AFTER class - taller corrections

--- clase01/Ejercicios.hs
+++ clase01/Ejercicios.hs
@@ -1,6 +1,6 @@
 module Ejercicios where
 
-import Clase01
+import Clase
 
 -- 1 absoluto: calcula el valor absoluto de un numero entero.
 absoluto :: Int -> Int
@@ -16,14 +16,24 @@ maximoEntreTresEnteros x y z  | maximo x y < z = z
                               | maximo z x < y = y
                               | maximo y z < x = x
 -- 4 algunoEs0: dados dos numeros racionales, decide si alguno de los dos es igual a 0 (hacerlo dos veces, una sin usar y otra usando pattern matching).
-algunoEsCeroConPattern :: Float -> Float -> Int
-algunoEsCeroConPattern x y  | x == 0.0 = 1
-                            | y == 0.0 = 1
-                            |otherwise = 0
+algunoEsCeroSinPattern :: Float -> Float -> Bool
+algunoEsCeroSinPattern x y  | x == 0 = True
+                            | y == 0 = True
+                            |otherwise = False
+
+algunoEsCeroConPattern :: Float -> Float -> Bool
+algunoEsCeroConPattern 0 _ = True
+algunoEsCeroConPattern _ 0 = True
+algunoEsCeroConPattern _ _ = False
 -- 5 ambosSon0: dados dos numeros racionales, decide si ambos son iguales a 0 (hacerlo dos veces, una sin usar y otra usando pattern matching).
-ambosSonCero :: Float -> Float -> Int
-ambosSonCero x y  | x == 0 && y == 0 = 1
-                  | otherwise = 0
+ambosSonCeroSinPattern :: Float -> Float -> Bool
+ambosSonCeroSinPattern x y = x == 0 && y == 0
+
+ambosSonCeroConPattern :: Float -> Float -> Bool
+ambosSonCeroConPattern 0 _ = True
+ambosSonCeroConPattern _ 0 = True
+ambosSonCeroConPattern _ _ = False
+
 -- 6 esMultiploDe: dados dos numeros naturales, decidir si el primero es multiplo del segundo.
 esMultiploDe :: Int -> Int -> Int
 esMultiploDe x y  | mod x y == 0 = 1
