2b0f2d8c8e1bef2ae0694c6db7ee8729e0ef29da
fjbernardez <bernardez.f@gmail.com>
2020-05-06 23:32:03 -0300

    ADD other exercises

---
+++ clase03/Ejercitacion.hs
@@ -0,0 +1,19 @@
+module Ejercitacion
+where
+  {-
+    este modulo completa la ejercitacion de la clase 3 junto con las funciones
+    añadias al modulo Naturales.hs en ./clase/Naturales.hs
+  -}
+
+  -- |Documentacion de tribonacci
+  -- tribonacci responde a la funcion partida dada como consigna de la clase 3, ejercico 1
+tribonacci :: Int -> Int
+tribonacci n  | n <= 2 = n
+              | otherwise = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3)
+
+-- |Documentacion de esMultiplo3
+-- esMultiplo3 indica si elñ argumento esmultiplo de 3
+esMultiplo3 :: Int -> Bool
+esMultiplo3 n | n == 0 = True
+              | n < 3 = False
+              | otherwise = esMultiplo3 (n - 3)
