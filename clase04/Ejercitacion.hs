1ca2238ac88de03aff624f51e2d689a479d0b08f
fjbernardez <bernardez.f@gmail.com>
2020-05-14 20:11:02 -0300

    ADD sumatoriaG1

--- clase04/Ejercitacion.hs
+++ clase04/Ejercitacion.hs
@@ -3,3 +3,9 @@ where
   {-
     este modulo es la ejercitacion de la clase 4
   -}
+
+-- |Documentacion de sumatoriaG1
+-- sumatoria que responde al ejercicio 4
+sumatoriaG1 :: (Int, Int) -> Int
+sumatoriaG1 (i,n) | i == n = i ^ (n)
+                  | otherwise = i ^ (n) + sumatoriaG1 (i,n-1)
