8422890b629b000aa2fe3a46ed553263f2080061
fjbernardez <bernardez.f@gmail.com>
2020-05-15 13:16:42 -0300

    ADD sumatoriaG2

--- clase04/Ejercitacion.hs
+++ clase04/Ejercitacion.hs
@@ -9,3 +9,21 @@ where
 sumatoriaG1 :: (Int, Int) -> Int
 sumatoriaG1 (i,n) | i == n = i ^ (n)
                   | otherwise = i ^ (n) + sumatoriaG1 (i,n-1)
+
+-- |Documentacion de sumatoriaG2
+-- sumatoria que responde al ejercicio 5
+-- sumatoriaG2 :: Int -> Int
+-- sumatoriaG2 0 = 0
+-- sumatoriaG2 n = sumatoriaG1 (pred n, n) + ??????????
+
+-- |Documentacion de sumatoriaG2
+-- sumatoria que responde al ejercicio 5
+sumatoriaG2 :: Int -> Int
+sumatoriaG2 0 = 0
+sumatoriaG2 n = sumatoriaG2 (n - 1) + sumatoriaG2Aux (n, 1)
+
+-- |Documentacion de sumatoriaG2Aux
+-- funcion creada para la implementacion de sumatoriaG2
+sumatoriaG2Aux :: (Int, Int) -> Int
+sumatoriaG2Aux (n, q) | n == q = n ^ n
+                      | otherwise = q ^ n + sumatoriaG2Aux (n , succ q)
