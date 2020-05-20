e415e36e5519f97054f8843cb7e1e38f9412f242
fjbernardez <bernardez.f@gmail.com>
2020-05-20 20:28:12 -0300

    ADD menorDivisor

--- clase05/clase/Clase.hs
+++ clase05/clase/Clase.hs
@@ -15,3 +15,17 @@ where
   -- sumaDivisores calcula la suma de los divisores un entero positivo
   sumaDivisores :: Int -> Int
   sumaDivisores n = sumaDivisoresHasta n n
+
+
+  -- |Documentacion de menorDivisorDesde
+  -- menorDivisorDesde devuelve el menor divisor de un numero entero n, comenzando desde un natural k
+  menorDivisorDesde :: Int -> Int -> Int
+  menorDivisorDesde n k   | k > n = 0
+                          | k == 1 || k == n  = k
+                          | mod n k == 0 = k
+                          | otherwise = menorDivisorDesde n (k+1)
+
+  -- |Documentacion de menorDivisor
+  -- menorDivisor calcula el menor divisor (mayor que 1) de un natural n
+  menorDivisor :: Int -> Int
+  menorDivisor n = menorDivisorDesde n 2
