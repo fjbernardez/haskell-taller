94bf6237de8a73055599307bba9158292ef302bb
fjbernardez <bernardez.f@gmail.com>
2020-05-30 21:52:26 -0300

    ADJUST minor changes

--- clase06/Ejercitacion06.hs
+++ clase06/Ejercitacion06.hs
@@ -1,5 +1,6 @@
 module Ejercitacion06
 where
+  import Clase06
   {-
     este modulo es la ejercitacion de la clase 6
   -}
@@ -9,3 +10,8 @@ where
   productoria :: [Int] -> Int
   productoria [] = 1 --nulo multiplicativo
   productoria (x:xs) = x * productoria xs
+
+  -- |Documentacion de sumarN
+  -- sumarN dado un numero N y una lista xs, suma N a cada elemento de xs
+  -- sumarN :: Int -> [Int] -> [Int]
+  -- sumarN n lista | tail list == []
